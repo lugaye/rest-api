c07a7090736b20e360813e2e45bd3d3f59b87045
lugaye <160602222+lugaye@users.noreply.github.com>
2024-05-07 13:36:38 +0300

    Initial commit

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    description: String
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
